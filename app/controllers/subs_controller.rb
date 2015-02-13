05f084108c714e95328994a6f8ce8bc38c959c37
App Academy Student <appacademy@Apps-Mac-mini.local>
2015-02-13 11:24:05 -0500

    Finished Subs routes

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,4 +1,6 @@
 class SubsController < ApplicationController
+  before_action :is_mod?, :only => [:edit, :update]
+
   def new
     @sub = Sub.new
     render :new
@@ -6,6 +8,18 @@ class SubsController < ApplicationController
   end
 
   def create
+    user = current_user
+    if user
+      @sub = Sub.new(sub_params)
+      @sub.moderator_id = user.id
+      if @sub.save
+        redirect_to sub_url(@sub)
+      else
+        render :new
+      end
+      else
+        redirect_to new_session_url
+      end
 
   end
 
@@ -15,15 +29,33 @@ class SubsController < ApplicationController
   end
 
   def show
+    @sub = Sub.find(params[:id])
+    render :show
 
   end
 
   def edit
+    @sub = Sub.find(params[:id])
+    render :edit
 
   end
 
   def update
+    @sub = Sub.find(params[:id])
+    if @sub.update(sub_params)
+      redirect_to sub_url(@sub)
+    else
+      render :edit
+    end
 
   end
 
+  private
+  def sub_params
+    params.require(:sub).permit(:title, :description)
+  end
+
+  def is_mod?
+    current_user.id == Sub.find(params[:id]).moderator_id
+  end
 end
